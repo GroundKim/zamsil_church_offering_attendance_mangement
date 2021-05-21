a311b129b15d6f30c5e293d4f9eb5ec529822775
jisangkim <jjoo0815@gmail.com>
2021-05-21 17:26:13 +0900

    select correct header with current URL

--- frontend/src/components/static/Header.vue
+++ frontend/src/components/static/Header.vue
@@ -5,7 +5,7 @@
       dense
       dark
     >
-      <v-tabs class="d-flex justify-center">
+      <v-tabs class="d-flex justify-center" v-model="activeTab">
         <v-tab
           v-for="(link, index) in links"
           :key="index"
@@ -22,27 +22,37 @@
 <script>
 export default {
   data: () => ({
+    activeTab: 1,
     links: [
       {
         icon: 'clipboard-edit',
         title: '출석부 기입',
         target: 'attendanceInput'
       },
+
       {
         icon: 'clipboard-outline',
         title: '출석부 보기',
         target: 'attendanceView'
       },
+
       {
         icon: 'book-edit',
         title: '헌금 기입',
         target: 'offeringInput'
       },
+
       {
         icon: 'book-outline',
         title: '헌금 보기',
         target: 'offeringView'
       },
+
+      {
+        icon: 'domain',
+        title: '재적 관리',
+        target: 'memberManagement'
+      }
     ]
   }),
 
@@ -52,10 +62,20 @@ export default {
         this.headerName = link.title
         this.$router.push({name: link.target}).catch(() => {})
       },
+
+      selectHeader(routeName) {
+        const index = this.links.findIndex(l => l.target === routeName)
+        this.activeTab = index
+      }
     },
 
     mounted () {
       this.headerName = this.$store.state.headerName
+      // avoid selecting wrong header v-tab when refreishing from client
+      this.selectHeader(this.$router.currentRoute.name)
+      document.title = '잠실교회 소년부 관리'
     },
+
+
 }
 </script>
