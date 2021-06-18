dfb39a37e5e6e66d8fc7ad34f23a32ce1f5cb5af
groundKim <jjoo0815@gmail.com>
2021-06-18 17:47:51 +0900

    add protocol handlers

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,8 @@ import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
-const protocol = 'http'
+//const protocol = 'http'
+const protocol = process.env.VUE_APP_PROTOCOL
 const address = process.env.VUE_APP_SERVERADDRESS
 const port = process.env.VUE_APP_PORT
 
