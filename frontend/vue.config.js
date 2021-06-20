5206d936f69b9bd656a9a526ffb3ef939589baaa
groundKim <jjoo0815@gmail.com>
2021-06-21 00:49:34 +0900

    set title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "잠실교회 소년부";
+            return args;
+        })
+    }
+
 }
