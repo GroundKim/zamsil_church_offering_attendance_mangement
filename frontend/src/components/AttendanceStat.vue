dc728b0d0cab2a6b9757beba7de3c79556d5b283
jisangkim <jjoo0815@gmail.com>
2021-07-08 17:44:05 +0900

    making attendanceStat

--- frontend/src/components/AttendanceStat.vue
+++ frontend/src/components/AttendanceStat.vue
@@ -5,7 +5,7 @@
     </v-row>
     
     <v-row>
-      <barGraph/>
+      <barGraph :numberData="numberData"></barGraph>
     </v-row>
   </v-container>
 </template>
@@ -21,20 +21,28 @@ export default {
   data() {
     return {
       year: null,
-      data: null,
+      numberData: null,
     }
   },
+
   
-  mounted () {
-    this.renderChart()
-    
+  beforeMount: function () {    
     //get year from url parameter
     this.year = this.$route.query.year
     axios
       .get(`${this.$serverAddress}/Youth/attendance/view/statistic/number/month?year=${this.year}`, {withCredentials: true})
       .then((res) => {
-        this.data = res.data
-        console.log(this.data);
+        this.numberData = res.data
+        console.log(JSON.stringify(this.numberData));
+        
+              
+      this.numberData.forEach(d => {
+        this.chartdata.datasets[0].data.push(d.numberOfDepartmentOne)
+        this.chartdata.datasets[1].data.push(d.numberOfDepartmentTwo)
+      })
+      console.log(this.chartdata.datasets[0]);
+    
+
       })
       .catch((err) => {
         err
--- frontend/src/components/chart/attendanceBarGraph.vue
+++ frontend/src/components/chart/attendanceBarGraph.vue
@@ -1,29 +1,71 @@
 <script>
-import { Bar } from 'vue-chart-3'
+import { Bar } from 'vue-chartjs'
 
 export default {
   extends: Bar,
+  props: {
+    numberData: {
+      type: Array,
+      default: null
+    },
+  },
+
   data: () => ({
     chartdata: {
-      labels: ['January', 'February'],
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [
         {
-          label: 'Data One',
+          label: '1부 인원',
+          data: [],
           backgroundColor: '#f87979',
-          data: [40, 20]
+        },
+
+        {
+          label: '2부 인원',
+          data: [],
+          backgroundColor: '#4B89DC',
         }
       ]
     },
+  
     options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            max: Math.max(40),
+            beginAtZero: true
+          }
+        }],
+
+        xAxes: [{
+          stacked:false,
+        }],
+       
+      },
+
+      legend: {
+        position: 'bottom'
+      },
+
+      title: {
+        display: true,
+        text: '소년부 월 별 출석 인원수',
+        font: { weight: 'bold'},
+        fontSize: 30,
+        padding: 15
+      },
       responsive: true,
-      maintainAspectRatio: false
     }
   }),
 
+  watch: {
+
+  },
+
   mounted () {
+    console.log(JSON.stringify(this.numberData));
     this.renderChart(this.chartdata, this.options)
   }
 }
+
 </script>
-
-
